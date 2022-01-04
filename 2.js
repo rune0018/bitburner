95980800bd6d931bdc40b98d6c18f750d8bfce11
rune0018 <rune00nielsen@gmail.com>
2022-01-04 13:14:29 +0100

    lol

--- 2.js
+++ 2.js
@@ -2,10 +2,10 @@ export async function main(ns) {
     var t = 0
     var hostname = ns.args
     while(true){
-        await ns.hack(hostname)
+        await ns.hack(hostname[0])
         if(t == 10){
-            await ns.grow(hostname)
-            await ns.weaken(hostname)
+            await ns.grow(hostname[0])
+            await ns.weaken(hostname[0])
             t = 0
         }
     	t++
