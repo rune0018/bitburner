954ede830bbd043eced2e94f069dc5b8a1f9a93a
rune0018 <rune00nielsen@gmail.com>
2022-01-04 08:10:59 +0100

    layer 1 and 2 auto inject

--- 1.js
+++ 1.js
@@ -1,18 +1,27 @@
 //bitburner script in javascript
 export async function main(ns) {
-    var s1 = true;
     while(true){
         var scan = ns.scan();
         for(let i = 0; i < scan.length; i++){
-            if(!await ns.hasRootAccess(scan[i])){
-                await ns.nuke(scan[i])
-                s1 = true;
+            var deepScan = ns.Scan(scan[i]);
+            for(let j = 0; j < deepScan.length; j++){
+                if(!await ns.hasRootAccess(scan[i])&&await ns.getServerNumPortsRequired(scan[i]) == 0 && await ns.getServerRequiredHackingLevel(host)< await ns.getHackingLevel()){
+                    await ns.nuke(scan[i])
+                }
+                if(await ns.hasRootAccess(deepScan[j])){
+                    await ns.wget("https://raw.githubusercontent.com/rune0018/bitburner/master/2.js", "2.js",scan[i]);
+                    await ns.exec("2.js", scan[i])
+                }
+                await ns.sleep(100)
             }
-            else if(s1){
+            if(!await ns.hasRootAccess(scan[i])&&await ns.getServerNumPortsRequired(scan[i]) == 0 && await ns.getServerRequiredHackingLevel(host)< await ns.getHackingLevel()){
+                await ns.nuke(scan[i])
+            }
+            if(await ns.hasRootAccess(scan[i])){
                 await ns.wget("https://raw.githubusercontent.com/rune0018/bitburner/master/2.js", "2.js",scan[i]);
                 await ns.exec("2.js", scan[i]);
-                s1 = false;
             }
+            await ns.sleep(100);
         }
         await ns.sleep(1000);
     }
