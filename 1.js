b6dea3a0343eaabfb900b25eb9407f871179ddd0
rune0018 <rune00nielsen@gmail.com>
2022-01-04 09:08:13 +0100

    new stuff

--- 1.js
+++ 1.js
@@ -1,29 +1,33 @@
 //bitburner script in javascript
 export async function main(ns) {
-    while(true){
+    var cycles = 5;
+    while(cycles>1){
         var scan = ns.scan();
         for(let i = 0; i < scan.length; i++){
-            var deepScan = ns.Scan(scan[i]);
-            for(let j = 0; j < deepScan.length; j++){
-                if(!await ns.hasRootAccess(scan[i])&&await ns.getServerNumPortsRequired(scan[i]) == 0 && await ns.getServerRequiredHackingLevel(host)< await ns.getHackingLevel()){
-                    await ns.nuke(scan[i])
-                }
-                if(await ns.hasRootAccess(deepScan[j])){
-                    await ns.wget("https://raw.githubusercontent.com/rune0018/bitburner/master/2.js", "2.js",scan[i]);
-                    await ns.exec("2.js", scan[i])
-                }
-                await ns.sleep(100)
-            }
+            //var deepScan = ns.scan(scan[i]);
+            //for(let j = 0; j < deepScan.length; j++){
+            //    if(!await ns.hasRootAccess(scan[i])&&await ns.getServerNumPortsRequired(scan[i]) == 0 && await ns.getServerRequiredHackingLevel(host)< await ns.getHackingLevel()){
+            //        await ns.nuke(deepScan[i])
+            //    }
+            //    if(await ns.hasRootAccess(deepScan[j])&& await ns.getScriptRam('2.js', deepScan[j])< 1){
+            //        await ns.wget("https://raw.githubusercontent.com/rune0018/bitburner/master/2.js", "2.js",scan[i]);
+            //        await ns.exec("2.js", deepScan[i])
+            //    }
+            //    await ns.sleep(100)
+            //}
             if(!await ns.hasRootAccess(scan[i])&&await ns.getServerNumPortsRequired(scan[i]) == 0 && await ns.getServerRequiredHackingLevel(host)< await ns.getHackingLevel()){
                 await ns.nuke(scan[i])
             }
-            if(await ns.hasRootAccess(scan[i])){
-                await ns.wget("https://raw.githubusercontent.com/rune0018/bitburner/master/2.js", "2.js",scan[i]);
-                await ns.exec("2.js", scan[i]);
+            if(await ns.hasRootAccess(scan[i])&& await ns.getScriptRam('1.js', scan[i])< 1){
+                await ns.wget("https://raw.githubusercontent.com/rune0018/bitburner/master/1.js", "1.js",scan[i]);
+                await ns.exec("1.js", scan[i]);
+                cycles++;
             }
+
             await ns.sleep(100);
         }
+        cycles--;
         await ns.sleep(1000);
     }
-    
+    await ns.run('2.js') 
 }
